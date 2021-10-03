fa838f4d2dfa17a1e2d8924716cde91e76fea74e
AngelCosmic <ogecrom12@gmail.com>
2021-10-03 19:53:16 -0300

    SegundoSave

--- Projeto-Tarefa/backend/src/routes.ts
+++ Projeto-Tarefa/backend/src/routes.ts
@@ -1,5 +1,5 @@
 import { Router, request, response, Request, Response} from 'express'
-import { getCadastros, saveCadastro, getCadastro } from './controller/CadastroController'
+
  
 import { getTasks, saveTask, getTask, updateTask, deleteTask, finishedTask } from './controller/tasksController'
 
@@ -16,9 +16,6 @@ routes.put('/tasks/:id', updateTask)
 routes.delete('/tasks/:id', deleteTask)
 routes.patch('/tasks/:id', finishedTask)
 
-
-routes.get('/cadastro', getCadastros)
-routes.post('/cadastro', saveCadastro)
 
 
  
